3d1e581213b48679f1d8ed2ca3a4bd820bf97a03
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-04-20 00:13:38 +0500

    initial layout

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,10 +1,26 @@
 import styles from './style.module.css'
 
-function Weather () {
+function Weather() {
     return (
         <div className={styles.container}>
-            <header><p className={styles.logo}>the.weather</p></header>
+            <header>
+                <p className={styles.logo}>the.weather</p>
+            </header>
+            <div className={styles.details}>
+                <h1>26&deg;</h1>
+                <div className={styles.selector}>
+                <span>
+                    <p>London</p>
+                    <p>10:34-Tuesday, 22 Oct &apos;19</p>
+                </span>
+                <span>
+                    <p className={styles.sun}>&#9788;</p>
+                    <p>Sunny</p>
+                </span>
+                </div>
+            </div>
         </div>
     )
 }
+
 export default Weather
