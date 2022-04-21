0b1e94f579e79d1ddc4ad415553ccb9e43b6dfca
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-04-21 05:32:58 +0500

    added initial redux

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import './App.css';
 // import {useSelector, useDispatch} from 'react-redux'
 import Weather from './components/Weather'
 import WeatherDetails from './components/WeatherDetails'
+import store from './redux/store'
 function App() {
-  // const counter = useSelector((state) => state.counter)
-  // const dispatch = useDispatch()
-  // const increment = () => {
-  //     dispatch({type: 'INC'})
-  // }
-  // const decrement = () => {
-  //     dispatch({type: 'DEC'})
-  // }
-  // const addBy = () => {
-  //   dispatch({type: 'ADD', payload: 10})
-  // }
+    console.log(store)
     return (
     <div className="container">
         <Weather/>
---
+++ src/redux/reducer.js
@@ -0,0 +1,10 @@
+import {LOCATION_CHANGED} from './actionType'
+// eslint-disable-next-line no-unused-vars
+let reducer = (state=[], action) => {
+    switch(action.type){
+        case LOCATION_CHANGED:
+        default:
+            return state
+    }
+}
+export default reducer
